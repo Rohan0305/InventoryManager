05953ba0cf7da7551d4ea21260b46ac190b6c1b3
Rohan <rohanvallapalli@gmail.com>
2024-06-11 01:24:23 -0400

    Edit Product

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -8,14 +8,46 @@ const router = express.Router();
 
 router.get("/get-products", async (req, res) => {
     try {
-      const user = await UserModel.findById(req.body.userId);
+      const userId = req.query.userId; 
+      const user = await UserModel.findById(userId);
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
       const products = await ProductModel.find({
         _id: { $in: user.products },
       });
   
-      res.status(201).json({ products });
+      res.status(200).json({ products }); 
     } catch (err) {
       console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get("/get-product/:productId", async (req, res) => {
+    try {
+      const product = await ProductModel.findById(req.params.productId);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      res.status(200).json(product); 
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.put("/update-product/:productId", async (req, res) => {
+    try {
+      const productId = req.params.productId;
+      const updatedProduct = await ProductModel.findByIdAndUpdate(productId, req.body, { new: true });
+      if (!updatedProduct) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      res.status(200).json(updatedProduct);
+    } catch (err) {
       res.status(500).json(err);
     }
   });
